582507da7c105668a268dcce182526a5de19ad60
YannC95 <yannm95@hotmail.fr>
2024-01-29 10:08:00 +0100

    init project

---
+++ src/main/java/com/isika/DemoEbsApplication.java
@@ -0,0 +1,13 @@
+package com.isika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEbsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/isika/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.isika.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping
+	public String helloWorld() {
+		return "{'message': 'Hello World! V1'}";
+	}
+}
